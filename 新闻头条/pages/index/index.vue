1e389b08f6d89ea7b4c7f32ff09c153f1c8cc939
blueskyk <1419293032@qq.com>
2021-01-06 22:19:06 +0800

    项目初始化

---
+++ 新闻头条/pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="content">
+		主页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
